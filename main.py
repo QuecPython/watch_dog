d53b8e643e7982613cbd287a950a58dd9ca2cd50
rivern.yuan <1520483071@qq.com>
2023-10-20 11:03:23 +0800

    edit main.py

--- main.py
+++ main.py
@@ -29,45 +29,45 @@ queue = Queue()
 
 # app_thread_1
 def app_thread_1():
-    print("[%s] Thread started." % __name__)
-    print("[%s] Create a software watchdog." % __name__)
-    print("[%s] We pretend that the thread will block for up to 5 seconds during normal operation." % __name__)
+    print("[%s] Thread started." % "app_thread_1")
+    print("[%s] Create a software watchdog." % "app_thread_1")
+    print("[%s] We pretend that the thread will block for up to 5 seconds during normal operation." % "app_thread_1")
     wdg = WDG.create(5)
 
     for i in range(10):
-        print("[%s] Feed the watchdog." % __name__)
+        print("[%s] Feed the watchdog." % "app_thread_1")
         wdg.feed()
 
-        print("[%s] Send a message to queue: %s." % (__name__, "A message from app_thread_1"))
-        queue.put(None)
+        print("[%s] Send a message to queue: %s." % ("app_thread_1", "A message from app_thread_1"))
+        queue.put("A message from app_thread_1")
 
         if i < 9:
-            print("[%s] Will feed the watchdog again after sleeping for 5 seconds." % __name__)
+            print("[%s] Will feed the watchdog again after sleeping for 5 seconds." % "app_thread_1")
             utime.sleep(5)
         else:
-            print("[%s] Sleep for a long time to simulate a scenario." % __name__)
+            print("[%s] Sleep for a long time to simulate a scenario" % "app_thread_1")
             print("    where an abnormality occurs and the dog cannot be fed in time.")
             utime.sleep(1800)
 
 # app_thread_2
 def app_thread_2():
-    print("[%s] Thread started." % __name__)
-    print("[%s] Create a software watchdog." % __name__)
-    print("[%s] We pretend that the thread will block for up to 15 seconds during normal operation." % __name__)
+    print("[%s] Thread started." % "app_thread_2")
+    print("[%s] Create a software watchdog." % "app_thread_2")
+    print("[%s] We pretend that the thread will block for up to 15 seconds during normal operation." % "app_thread_2")
     wdg =  WDG.create(15, queue)
 
     while True:
-        print("[%s] Feed the watchdog." % __name__)
+        print("[%s] Feed the watchdog." % "app_thread_2")
         wdg.feed()
 
-        print("[%s] Waiting for a message from queue." % __name__)
-        print("[%s] Will feed the watchdog again after getting the message." % __name__)
+        print("[%s] Waiting for a message from queue." % "app_thread_2")
+        print("[%s] Will feed the watchdog again after getting the message." % "app_thread_2")
 
         msg = queue.get()
         if msg:
-            print("[%s] Got the message: %s." % (__name__, msg))
+            print("[%s] Got the message: %s." % ("app_thread_2", msg))
         else:
-            print("[%s] Got a None message from the underlying watchdog mechanism." % __name__)
+            print("[%s] Got a None message from the underlying watchdog mechanism." % "app_thread_2")
 
 
 # Start Child thread test 1
